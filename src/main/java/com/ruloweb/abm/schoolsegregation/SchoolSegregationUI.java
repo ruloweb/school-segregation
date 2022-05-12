0a0b06d8e1700f0864120adca5fb084969519c42
Jose Sanchez <jose.sanchez@wildlifestudios.com>
2022-05-11 22:54:48 -0300

    Show schools by color

--- src/main/java/com/ruloweb/abm/schoolsegregation/SchoolSegregationUI.java
+++ src/main/java/com/ruloweb/abm/schoolsegregation/SchoolSegregationUI.java
@@ -1,14 +1,17 @@
 package com.ruloweb.abm.schoolsegregation;
 
 import com.ruloweb.abm.schoolsegregation.agent.Household;
+import com.ruloweb.abm.schoolsegregation.agent.HouseholdType;
 import com.ruloweb.abm.schoolsegregation.agent.School;
 import sim.display.Console;
 import sim.display.Controller;
 import sim.display.Display2D;
 import sim.display.GUIState;
 import sim.engine.SimState;
+import sim.portrayal.DrawInfo2D;
 import sim.portrayal.continuous.ContinuousPortrayal2D;
 import sim.portrayal.simple.OvalPortrayal2D;
+import sim.portrayal.simple.ShapePortrayal2D;
 
 import javax.swing.*;
 import java.awt.*;
@@ -48,9 +51,35 @@ public class SchoolSegregationUI extends GUIState {
 
         // tell the portrayals what to portray and how to portray them
         schoolsPortrayal.setField(model.fieldSchools);
-        schoolsPortrayal.setPortrayalForClass(School.class, new OvalPortrayal2D(Color.RED));
+        schoolsPortrayal.setPortrayalForClass(
+                School.class,
+                new ShapePortrayal2D(ShapePortrayal2D.X_POINTS_TRIANGLE_UP, ShapePortrayal2D.Y_POINTS_TRIANGLE_UP, 5.0, true) {
+                    @Override
+                    public void draw(Object object, Graphics2D graphics, DrawInfo2D info) {
+                        School school = (School)object;
+
+                        if (school.getTotal() == 0) {
+                            paint = Color.BLACK;
+                        }
+                        else {
+                            int red = (int)(255 * school.getRed() / school.getTotal());
+                            int blue = (int)(255 * school.getBlue() / school.getTotal());
+                            paint = new Color(red, 0, blue);
+                        }
+
+                        super.draw(object, graphics, info);
+                    }
+                }
+        );
         householdersPortrayal.setField(model.fieldHouseholders);
-        householdersPortrayal.setPortrayalForClass(Household.class, new OvalPortrayal2D());
+        householdersPortrayal.setPortrayalForClass(Household.class, new OvalPortrayal2D() {
+            @Override
+            public void draw(Object object, Graphics2D graphics, DrawInfo2D info) {
+                Household household = (Household)object;
+                paint = household.getType() == HouseholdType.RED ? new Color(0xFF5555) : new Color(0x5555FF);
+                super.draw(object, graphics, info);
+            }
+        });
 
         // reschedule the displayer
         display.reset();
